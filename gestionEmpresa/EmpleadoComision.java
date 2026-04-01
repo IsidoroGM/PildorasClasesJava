4f5fe1e244c81ce631a35231c86ccf7744a06226
Iisidoro Gordillo <igm27292@gmail.com>
2026-04-01 14:06:41 +0200

    Añadir clase EmpleadoComision y clase principal GesionEmpresaMain

---
+++ gestionEmpresa/EmpleadoComision.java
@@ -0,0 +1,52 @@
+public class EmpleadoComision extends Empleado {
+
+
+    public EmpleadoComision(){
+        super();
+        clientesCaptados=0;
+        comisionPorCliente=0;
+    }
+
+    public EmpleadoComision(String nombre, String DNI, int agnoAlta, int yearIngreso, int clientesCaptados, double comisionPorCliente){
+        super(nombre, DNI, agnoAlta);
+        this.clientesCaptados=clientesCaptados;
+        this.comisionPorCliente=comisionPorCliente;
+
+    }
+
+    @Override
+    public double getSalario() {
+        if(comisionPorCliente*clientesCaptados>salarioMinimo){
+            return comisionPorCliente*clientesCaptados;
+        } else return salarioMinimo;
+    }
+
+    @Override
+    public void imprimir(){
+        super.imprimir();
+        System.out.printf("Salario: %.2f\n", getSalario());
+        System.out.println();
+    }
+
+    public void setClientesCaptados(int clientesCaptados){
+        this.clientesCaptados=clientesCaptados;
+    }
+
+    public int getClientesCaptados(){
+        return clientesCaptados;
+    }
+
+    public void setComisionPorCliente(double comisionPorCliente){
+        this.comisionPorCliente=comisionPorCliente;
+    }
+
+    public double getComisionPorCliente(){
+        return comisionPorCliente;
+    }
+
+
+    private int clientesCaptados;
+    private double comisionPorCliente;
+    public static final double salarioMinimo=950;
+
+}
---
+++ gestionEmpresa/GesionEmpresaMain.java
@@ -0,0 +1,12 @@
+public class GesionEmpresaMain {
+
+    public static void main(String[] args) {
+        
+        Empleado empleados[]=new Empleado[4];
+
+        EmpleadoAsalariado EA1=new EmpleadoAsalariado("Antonio", "49120270F", 1125, 2020);
+        empleados[0]=EA1;
+        
+    }
+
+}

022b12098a32e49ea2be50d5cbd37f90e9f01ffc
Iisidoro Gordillo <igm27292@gmail.com>
2026-06-08 23:46:27 +0200

    estudio 08/06 Retomando el estudio

---
+++ ClasesInternas/ClasesInternas.java
@@ -0,0 +1,71 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+
+
+//Estudiaremos las clases internas, que son clases que se definen dentro de otra clase.
+
+/*
+CLASES INTERNAS
+ *Para acceder a los campos privados de una clase dentro de otra
+ *Para ocultar una clase de las pertenecientes al mismo paquete
+ *Oara crear ckases internas "anonimas", muy útiles para gestionar eventos y retrollamadas
+ *Cuando solo una clase debe acceder a los campos de ejemplar de otra clase
+ */
+
+ // Para realizar el ejemplo, volveremos a realizar un temporizador.
+
+public class ClasesInternas {
+
+    public static void main(String[] args) {
+
+        Reloj mireloj=new Reloj(3000, true);
+        mireloj.ejecutarTemporizador();
+
+        JOptionPane.showMessageDialog(null, "Pulsa aceptar para terminar");
+        
+    }
+
+}
+
+class Reloj{
+
+    public Reloj(int intervalo, boolean sonido){
+        this.intervalo=intervalo;
+        this.sonido=sonido;
+    }
+
+    public void ejecutarTemporizador(){
+        ActionListener oyente=new DameLaHora();
+
+        Timer miTemporizador= new Timer(intervalo,oyente);
+        miTemporizador.start();
+    }
+
+    //Campo que realiza el conteo temporal.
+    private int intervalo;
+
+    //Campo que recoge el sonido.
+    private boolean sonido;
+
+    //CLASE INTERNA
+    //Solo puede aplicarse el modificador PRIVATE a una clase cuando es clase interna
+    private class DameLaHora implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            Date ahora=new Date();
+
+            System.out.println("Te pongo la hora cada 3 segundos" + ahora);
+        }
+
+        
+
+    }
+}
+
+

8bf8ce4ec34f828c19c266b79e04635d2011c7ae
Isidoro Gordillo <igm27292@gmail.com>
2026-06-14 17:09:34 +0200

    Añadir importación de Border y mejorar la estructura del código en EscribiendoEnMarcos

--- InterfacesGraficas/Interfaces/EscribiendoEnMarcos.java
+++ InterfacesGraficas/Interfaces/EscribiendoEnMarcos.java
@@ -3,6 +3,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class EscribiendoEnMarcos {
 
@@ -14,41 +15,63 @@ public class EscribiendoEnMarcos {
     }
 }
 
+// Creamos clase para el JFrame
 class MarcoTexto extends JFrame{
     public MarcoTexto(){
+            
+            setBounds(400,200,600,450);
+            setTitle("Escritura");
+            setVisible(true);
 
-        setBounds(400,200,600,450);
-        setTitle("Escritura");
-        setVisible(true);
+            // PrimerPanel milamina=new PrimerPanel();
+            // add(milamina);
 
-        PrimerPanel milamina=new PrimerPanel();
-        add(milamina);
+            add(new JPanel(){
+
+                public void paintComponent(Graphics g){
+
+                 //Pero para que el metodo pinte el componente, 
+                 //debemos llamar al metodo paintComponent de la clase padre JPanel 
+                 // y hara el trabajo que tenga asignado el metodo paintComponent
+
+                setBackground(new Color(39, 245, 242));
+
+                super.paintComponent(g);
+                
+                // Creamos un color para la letra
+                g.setColor(new Color(224, 0, 36));
+
+                g.setFont(new Font("Verdana", Font.ITALIC, 30));
+
+                g.drawString("Hola amigos", 200, 200);
+            }
+        });    
 
     }
-}
+}    
 
 // Craemos clase para poder escribir en el JFrame
 // El tamaño del panel se adapta or defecto al JFrame
 
-class PrimerPanel extends JPanel{
+// class PrimerPanel extends JPanel{
 
-    public void paintComponent(Graphics g){
+//     public void paintComponent(Graphics g){
 
-        //Pero para que el metodo pinte el componente, 
-        //debemos llamar al metodo paintComponent de la clase padre JPanel 
-        // y hara el trabajo que tenga asignado el metodo paintComponent
+//         //Pero para que el metodo pinte el componente, 
+//         //debemos llamar al metodo paintComponent de la clase padre JPanel 
+//         // y hara el trabajo que tenga asignado el metodo paintComponent
 
-        super.paintComponent(g);
+//         super.paintComponent(g);
 
-        setBackground(new Color(39, 245, 242));
+//         setBackground(new Color(39, 245, 242));
 
-        Color miLetraColor=new Color(224, 0, 36);
-        g.setColor(miLetraColor);
+//         Color miLetraColor=new Color(224, 0, 36);
+//         g.setColor(miLetraColor);
 
-        Font miletra=new Font("Verdana", Font.ITALIC, 30);
+//         Font miletra=new Font("Verdana", Font.ITALIC, 30);
 
-        g.setFont(miletra);
-        g.drawString("Hola amigos", 20, 20);
+//         g.setFont(miletra);
+//         g.drawString("Hola amigos", 20, 20);
         
-    }
-}
+//     }
+// }

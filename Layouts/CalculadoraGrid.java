2c5df38b64dac1f42b61e6490fa432b9e820af7a
Iisidoro Gordillo <igm27292@gmail.com>
2026-07-14 18:30:40 +0200

    Refactor PanelCalculadora para usar JTextField en lugar de JButton para la pantalla y mejorar la disposición de los botones.

--- Layouts/CalculadoraGrid.java
+++ Layouts/CalculadoraGrid.java
@@ -4,6 +4,8 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
 
@@ -33,19 +35,51 @@ class CalculadoraGridFrame extends JFrame{
 
 class PanelCalculadora extends JPanel{
 
+    private JPanel numeracion;
+
     public PanelCalculadora(){
 
         setLayout(new BorderLayout());
 
-        JButton pantalla=new JButton("0");
-        pantalla.setEnabled(false);
+        JTextField pantalla=new JTextField("0");
+        pantalla.setEditable(false);
+        pantalla.setHorizontalAlignment(SwingConstants.RIGHT);
+
         add(pantalla, BorderLayout.NORTH);
-
-        JPanel numeracion=new JPanel();
-        numeracion.setLayout(new GridLayout(4, 4));
         
+        numeracion = new JPanel();
+        numeracion.setLayout(new GridLayout(4, 4, 4, 5));
+        
+        declararBoton("7");
+        declararBoton("8");
+        declararBoton("9");
+        declararBoton("x");
+        declararBoton("4");
+        declararBoton("5");
+        declararBoton("6");
+        declararBoton("-");
+        declararBoton("1");
+        declararBoton("2");
+        declararBoton("3");
+        declararBoton("+");
+        declararBoton("0");
+        declararBoton(",");
+        declararBoton("=");
+        declararBoton("");
 
+        add(numeracion, BorderLayout.CENTER);
+        
     }
+
+    private void declararBoton(String textoBoton){
+
+        JButton boton=new JButton(textoBoton);
+
+        numeracion.add(boton);
+    }
+
+
 }
+
 
 

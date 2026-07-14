9f6bc97a687bc51cee8d72086d8d591888f4e7b9
Iisidoro Gordillo <igm27292@gmail.com>
2026-07-14 12:17:50 +0200

    Añadir clase EjemploFlowLayout con un JFrame y botones de colores utilizando BorderLayout

---
+++ Layouts/EjemploFlowLayout.java
@@ -0,0 +1,51 @@
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class EjemploFlowLayout {
+
+    public static void main(String[] args) {
+
+        MarcoConLayout miMarco=new MarcoConLayout();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miMarco.setVisible(true);
+        
+    }
+}
+
+class MarcoConLayout extends JFrame{
+
+    public MarcoConLayout(){ 
+    
+        setTitle("Disposiciones");
+        setBounds(600, 350, 600, 300);
+
+        PanelConLayout lamina=new PanelConLayout();
+        add(lamina);
+
+
+    }
+}
+
+class PanelConLayout extends JPanel{
+
+    public PanelConLayout(){
+
+        //setLayout(new java.awt.FlowLayout(FlowLayout.CENTER, 50, 100));
+
+        setLayout(new BorderLayout());
+        
+        
+        add(new JButton("Amarillo"));
+        add(new JButton("Rojo"));
+        add(new JButton("Azul"));
+        add(new JButton("Verde"));
+        add(new JButton("Morado"));
+
+        
+        
+    }
+}

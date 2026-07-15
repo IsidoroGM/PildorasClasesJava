e377d5d76103d8a7be58587831432fa3989cf7a6
Iisidoro Gordillo <igm27292@gmail.com>
2026-07-15 13:34:27 +0200

    Refactor PanelCalculadora para implementar lógica de cálculo y manejo de eventos en botones numéricos y de operación.

--- Layouts/CalculadoraGrid.java
+++ Layouts/CalculadoraGrid.java
@@ -1,6 +1,8 @@
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -35,13 +37,22 @@ class CalculadoraGridFrame extends JFrame{
 
 class PanelCalculadora extends JPanel{
 
-    private JPanel numeracion;
+    private final JPanel numeracion;
+    private final JTextField pantalla;
+    private boolean comienzo;
+
+    //Variables de lógica de cálculo
+    private String ultimaOperacion="";
+    private double resultado;
+    private double ultimoValor;
+    
 
     public PanelCalculadora(){
 
         setLayout(new BorderLayout());
 
-        JTextField pantalla=new JTextField("0");
+        comienzo=true;
+        pantalla=new JTextField("0");
         pantalla.setEditable(false);
         pantalla.setHorizontalAlignment(SwingConstants.RIGHT);
 
@@ -50,36 +61,94 @@ class PanelCalculadora extends JPanel{
         numeracion = new JPanel();
         numeracion.setLayout(new GridLayout(4, 4, 4, 5));
         
-        declararBoton("7");
-        declararBoton("8");
-        declararBoton("9");
-        declararBoton("x");
-        declararBoton("4");
-        declararBoton("5");
-        declararBoton("6");
-        declararBoton("-");
-        declararBoton("1");
-        declararBoton("2");
-        declararBoton("3");
-        declararBoton("+");
-        declararBoton("0");
-        declararBoton(",");
-        declararBoton("=");
-        declararBoton("");
+
+        InsertarNumero insertar=new InsertarNumero();
+        Operacion realizarOperacion=new Operacion();
+
+        declararBoton("7",insertar);
+        declararBoton("8",insertar);
+        declararBoton("9",insertar);
+
+        declararBotonOperacion("x",realizarOperacion);
+
+        declararBoton("4",insertar);
+        declararBoton("5",insertar);
+        declararBoton("6",insertar);
+
+        declararBotonOperacion("-",realizarOperacion);
+
+        declararBoton("1",insertar);
+        declararBoton("2",insertar);
+        declararBoton("3",insertar);
+
+        declararBotonOperacion("+",realizarOperacion);
+
+        declararBoton("0",insertar);
+
+        declararBotonOperacion(",",realizarOperacion);
+        declararBotonOperacion("=",realizarOperacion);
+        declararBotonOperacion("/",realizarOperacion);
 
         add(numeracion, BorderLayout.CENTER);
         
     }
 
-    private void declararBoton(String textoBoton){
+    //Método para valores numéricos
+    private void declararBoton(String textoBoton, InsertarNumero oyente){
 
         JButton boton=new JButton(textoBoton);
+
+        boton.addActionListener(oyente);
 
         numeracion.add(boton);
     }
 
+    //Método para operaciones
+    private void declararBotonOperacion(String textoBoton, Operacion oyente){
 
+        JButton boton=new JButton(textoBoton);
+
+        boton.addActionListener(oyente);
+
+        numeracion.add(boton);
+    }
+
+    private class InsertarNumero implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            String  entrada=e.getActionCommand();
+
+            if(comienzo){
+                pantalla.setText(entrada);
+                comienzo=false;
+            } else{
+                pantalla.setText(pantalla.getText()+entrada);
+            }
+
+            ultimoValor=Double.parseDouble(pantalla.getText());
+
+        }
+    }
+
+    private class Operacion implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            String operacionPulsada=e.getActionCommand();
+            resultado+=ultimoValor;
+            pantalla.setText("");
+            comienzo=true;
+            
+
+        }
+
+    }
 }
+
+
 
 
 
